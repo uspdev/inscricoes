9ef0811861d80dc791ddb4264f3153cdd13383c6
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-08-14 09:38:33 -0300

    Migration de processos criada

---
+++ app/Http/Controllers/ProcessoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Processo;
+use Illuminate\Http\Request;
+
+class ProcessoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Processo  $processo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Processo $processo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Processo  $processo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Processo $processo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Processo  $processo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Processo $processo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Processo  $processo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Processo $processo)
+    {
+        //
+    }
+}
---
+++ app/Processo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Processo extends Model
+{
+    //
+}
