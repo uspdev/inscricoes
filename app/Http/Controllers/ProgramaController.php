d631f20d4b33381fb6c954a244a0e66650787fdf
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-08-19 18:32:13 -0300

    Migration Programas

---
+++ app/Http/Controllers/ProgramaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Programa;
+use Illuminate\Http\Request;
+
+class ProgramaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Programa $programa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Programa $programa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Programa $programa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Programa $programa)
+    {
+        //
+    }
+}
