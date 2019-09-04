5e17d7f0cc4362bf6263f8970c9ebbe6f3af18fd
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-09-04 14:28:24 -0300

    Controller para Vagas

--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Vaga;
+use App\Programa;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Uspdev\Replicado\Connection; 
+use Uspdev\Replicado\Posgraduacao; 
 
 class VagaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,9 @@ class VagaController extends Controller
      */
     public function index()
     {
-        //
+        $vagas = Vaga::all();
+
+        return view('vagas.index', compact('vagas'));
     }
 
     /**
@@ -24,7 +35,9 @@ class VagaController extends Controller
      */
     public function create()
     {
-        //
+        $vagas = Vaga::all();
+
+        return view('vagas.create', compact('vagas'));
     }
 
     /**
@@ -35,7 +48,21 @@ class VagaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $vaga = new Vaga;
+        $vaga->titulo           = $request->titulo;
+        $vaga->codcur           = $request->codcur;
+        $vaga->inicio           = Carbon::createFromFormat('d/m/Y H:i', $request->inicio . ' ' . $request->inicioTime)->format('Y-m-d H:i');
+        $vaga->fim              = Carbon::createFromFormat('d/m/Y H:i', $request->fim . ' ' . $request->fimTime)->format('Y-m-d H:i');
+        $vaga->niveis           = implode(',', array_filter(array($request->niveisME, $request->niveisDO, $request->niveisDD)));
+        $vaga->status           = $request->status;
+        $vaga->data_elaborado   = Carbon::now()->format('Y-m-d H:i');
+        $vaga->save();
+
+        $vagas = Vaga::all();
+        
+        $request->session()->flash('alert-success', 'Vaga cadastrada com sucesso!');
+
+        return view('vagas.index', compact('request', 'vagas'));
     }
 
     /**
@@ -46,7 +73,25 @@ class VagaController extends Controller
      */
     public function show(Vaga $vaga)
     {
-        //
+        $niveis = explode(',', $vaga->niveis);
+        $arrNiveis = array();
+        foreach ($niveis as $nivel) {
+            switch ($nivel) {
+                case 'ME':
+                    array_push($arrNiveis, 'Mestrado');
+                    break;
+                case 'DO':
+                    array_push($arrNiveis, 'Doutorado');
+                    break;
+                case 'DD':
+                    array_push($arrNiveis, 'Doutorado Direto');
+                    break;    
+            }
+        }
+        
+        $niveis = implode(', ', $arrNiveis);
+        
+        return view('vagas.show', compact('vaga', 'niveis'));
     }
 
     /**
@@ -57,7 +102,10 @@ class VagaController extends Controller
      */
     public function edit(Vaga $vaga)
     {
-        //
+        $programasReplicado = Posgraduacao::programas(config('inscricoes.repUnd'));
+        $programas = Programa::all();
+       
+        return view('vagas.edit', compact('vaga', 'programas', 'programasReplicado'));
     }
 
     /**
@@ -69,7 +117,24 @@ class VagaController extends Controller
      */
     public function update(Request $request, Vaga $vaga)
     {
-        //
+        $vaga->titulo       = $request->titulo;
+        $vaga->codcur       = $request->codcur;
+        $vaga->inicio       = Carbon::createFromFormat('d/m/Y H:i', $request->inicio . ' ' . $request->inicioTime)->format('Y-m-d H:i');
+        $vaga->fim          = Carbon::createFromFormat('d/m/Y H:i', $request->fim . ' ' . $request->fimTime)->format('Y-m-d H:i');
+        $vaga->niveis       = implode(',', array_filter(array($request->niveisME, $request->niveisDO, $request->niveisDD)));
+        if ($vaga->status != $request->status and $request->status == 'Publicado') {
+            $vaga->data_publicado = Carbon::now()->format('Y-m-d H:i');
+        } elseif ($vaga->status != $request->status and $request->status == 'Concluido') {
+            $vaga->data_concluido = Carbon::now()->format('Y-m-d H:i');
+        }       
+        $vaga->status       = $request->status;
+        $vaga->save();
+
+        $vagas = Vaga::all();
+        
+        $request->session()->flash('alert-success', 'Vaga alterada com sucesso!');
+
+        return view('vagas.index', compact('request', 'vagas'));
     }
 
     /**
@@ -78,8 +143,14 @@ class VagaController extends Controller
      * @param  \App\Vaga  $vaga
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Vaga $vaga)
+    public function destroy(Request $request, Vaga $vaga)
     {
-        //
+        $vaga->delete();
+
+        $vagas = Vaga::all();
+
+        $request->session()->flash('alert-danger', 'Vaga apagada!');
+
+        return view('vagas.index', compact('vagas'));
     }
 }
