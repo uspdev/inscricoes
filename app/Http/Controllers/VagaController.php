e8cf3e5d52b90f4f1d116986b5cdc0d1546f3a3c
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-09-04 14:10:24 -0300

    Model, Resource e Controller para Vagas

---
+++ app/Http/Controllers/VagaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vaga;
+use Illuminate\Http\Request;
+
+class VagaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vaga  $vaga
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vaga $vaga)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Vaga  $vaga
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vaga $vaga)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Vaga  $vaga
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vaga $vaga)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Vaga  $vaga
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vaga $vaga)
+    {
+        //
+    }
+}
